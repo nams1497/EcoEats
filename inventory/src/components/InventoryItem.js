319bb5c4e5dfe3f5fda48c9e822a9315b5baf519
namr0002 <namr0002@student.monash.edu>
2024-04-06 10:21:41 +1100

    Corrected InventoryItem.js Fixed merge conflicts

--- inventory/src/components/InventoryItem.js
+++ inventory/src/components/InventoryItem.js
@@ -17,28 +17,3 @@ function InventoryItem({ item }) {
 }
 
 export default InventoryItem;
-<<<<<<< HEAD
-
-
-// // InventoryItem.js
-// import React from 'react';
-
-// function InventoryItem({ item, onDelete }) { // Note the addition of the onDelete prop here
-//   return (
-//     <tr>
-//       <td>{item.name}</td>
-//       <td>{item.amount}</td>
-//       <td>{item.spent}</td>
-//       <td>{item.expiryDate}</td>
-//       <td>{item.status}</td>
-//       <td>
-//         <span className="delete-icon" onClick={() => onDelete(item.id)}>&times;</span>
-//         <span className="menu-icon">⋮</span>
-//       </td>
-//     </tr>
-//   );
-// }
-
-// export default InventoryItem;
-=======
->>>>>>> 24ce2c4 (inventory folder)
